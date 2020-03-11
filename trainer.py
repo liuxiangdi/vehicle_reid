2de7255292e48a6a32213cdbb59a117d7aee61f3
皮皮子 <1578608950@qq.com>
2020-03-11 16:06:59 +0800

    2

--- trainer.py
+++ trainer.py
@@ -11,7 +11,7 @@ class CCL_trainer():
         super().__init__()
         self.model = Vgg16Net()
         self.model.to(device)
-    
+    """
     def ccl_loss(self, pos_features, neg_features, margin=0.5):
         # 计算positive 中心点
         pos_center = pos_features.mean(0)
@@ -40,7 +40,64 @@ class CCL_trainer():
 
             total_loss += loss
         return loss
-    
+    """
+
+    def ccl_loss(self, positiveimageset, negetiveimageset, margin):
+        posbatchsize = len(positiveimageset)
+        negbatchsize = len(negetiveimageset)
+        posimgbatch = 0
+        negimgbatch = 0
+        for i in range(posbatchsize):
+            if i == 0:
+                posimgbatch = positiveimageset[0]
+            else:
+                posimgbatch = torch.cat([posimgbatch, positiveimageset[i]])
+        for i in range(negbatchsize):
+            if i == 0:
+                negimgbatch = negetiveimageset[0]
+            else:
+                negimgbatch = torch.cat([negimgbatch, negetiveimageset[i]])
+
+        posfeatures = self.model(posimgbatch)
+        negfeatures = self.model(negimgbatch)
+
+        centerfeature = 0
+        for i in range(len(posfeatures)):
+            if i == 0:
+                centerfeature = posfeatures[0]
+            else:
+                centerfeature = centerfeature + posfeatures[i]
+        centerfeature = torch.div(centerfeature, len(posfeatures))
+
+        #归一化
+        centerfeature = torch.div(centerfeature, torch.norm(centerfeature, 2))
+        centerfeature = torch.unsqueeze(centerfeature, 0)
+        #print(centerfeature.shape)
+
+        tempnegfeature = 0
+        harddistance = float('Inf')
+        hardnegefeature = Variable(torch.zeros(1, 128).cuda())
+        for i in range(len(negfeatures)):
+            tempnegfeature = negfeatures[i]
+            #归一化
+            tempnegfeature = torch.div(tempnegfeature, torch.norm(tempnegfeature, 2))
+            tempnegfeature = torch.unsqueeze(tempnegfeature, 0)
+            #print(tempnegfeature.shape)
+            dis = F.pairwise_distance(centerfeature, tempnegfeature, p=2).cpu().data.numpy()[0][0]
+            if dis < harddistance:
+                harddistance = dis
+                hardnegefeature = tempnegfeature
+
+        totalloss = 0
+        for i in range(len(posfeatures)):
+            temposfeature = posfeatures[i]
+            #归一化
+            temposfeature = torch.div(temposfeature, torch.norm(temposfeature, 2))
+            temposfeature = torch.unsqueeze(temposfeature, 0)
+            loss = F.pairwise_distance(temposfeature, centerfeature) + margin - F.pairwise_distance(centerfeature, hardnegefeature)
+            loss = torch.clamp(loss, min=0)
+            totalloss += loss
+        return totalloss
 
     def train(self):
         loader = CClDataLoader("train")
