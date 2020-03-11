b1ab55f8519dd07f02acff78d791973668b10d70
lxd <1578608950@qq.com>
2020-03-11 17:17:12 +0800

    add checkpoints

--- trainer.py
+++ trainer.py
@@ -1,4 +1,5 @@
 import torch
+import time
 import os
 import torch.optim as optim
 import torch.nn.functional as F
@@ -7,21 +8,34 @@ from dataloader import AIC20_dataloader_CCL
 from losss import ccl_loss
 
 device = torch.device("cuda:1")
-model_path = "/home/lxd/checkpoints"
+date = time.strftime("%m-%m", time.localtime())
+model_path = "/home/lxd/checkpoints/" + date
+if not os.path.exists(model_path):
+    os.makedirs(model_path)
 
 class CCL_trainer():
-    def __init__(self):
+    def __init__(self, model_name, data_name):
         super().__init__()
-        self.model = Vgg16Net()
+        self.model_name = model_name
+        self.data_name = data_name
+
+        if model_name == "Vgg16":
+            self.model = Vgg16Net()
         self.model.to(device)
-    
-    def train(self):
-        loader = AIC20_dataloader_CCL("train")
         
-        optimizer = optim.SGD(self.model.parameters(), lr=0.001, momentum=0.8)
+        if data_name == "AIC20":
+            self.loader_train = AIC20_dataloader_CCL("train")
+
+    def train(self):
+        lr = 0.001
+        optimizer = optim.SGD(self.model.parameters(), lr=lr, momentum=0.8)
         avg_loss = 0
-        for index in range(10000):
-            pos, neg = loader.get_batch()
+        for index in range(20000):
+            if index % 10000 == 0:
+                lr /= 2
+                optimizer = optim.SGD(self.model.parameters(), lr=lr, momentum=0.8)
+            
+            pos, neg = self.loader_train.get_batch()
             pos = torch.stack(pos).to(device)
             neg = torch.stack(neg).to(device)
 
@@ -35,13 +49,17 @@ class CCL_trainer():
             optimizer.step()
 
             avg_loss += loss.item()
-            if index % 50 == 0 and index != 0:
-                print('avgloss {}'.format(avg_loss/50))
-                avg_loss = 0
             if index % 2000 == 0 and index != 0:
-                path = os.path.join(model_path, "{}_{}".format(index, avg_loss))
+                path = os.path.join(model_path, "{}_{}_{}_{}".format(self.model_name, self.data_name, index, avg_loss))
                 torch.save(self.model.state_dict(), path)
+            if index % 100 == 0 and index != 0:
+                print('batch {}  avgloss {}'.format(index, avg_loss/100))
+                avg_loss = 0
+
+    def val(self):
+        pass
+
 
 if __name__ == "__main__":
-    trainer = CCL_trainer()
+    trainer = CCL_trainer(model_name = "Vgg16", data_name = "AIC20")
     trainer.train()
