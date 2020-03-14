dd7d919c6eecdd13e1d0521dbbac32c21cf1bcd5
皮皮子 <1578608950@qq.com>
2020-03-14 19:25:43 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,6 @@
 import numpy as np
 import torch
 
-
 def pdist(vectors):
     distance_matrix = -2 * vectors.mm(torch.t(vectors)) + vectors.pow(2).sum(dim=1).view(1, -1) + vectors.pow(2).sum(
         dim=1).view(-1, 1)
@@ -9,22 +8,28 @@ def pdist(vectors):
 
 def validation(features, labels):
     """
-    features [num x 128, num x 128..]
+    features [num x 128]
     """
-    dis = pdist(features)
-    print(dis)
-
+    diss = np.full([len(features), len(features)], float("Inf"), dtype=np.float)
+    for i in range(len(features)):
+        if i % 100 == 0:
+            print("cal_dis {} / {}".format(i, len(features)))
+        for j in range(i+1, len(features)):
+            dis = float(np.linalg.norm(features[i][0]-features[j][0], 2))
+            diss[i][j] = dis
+            diss[j][i] = dis
+    
     cmc = []
-    for i in range(len(dis)):
+    for i in range(len(diss)):
         anchor_label = labels[i]
-        dis[i][i] = float("inf")
+        diss[i][i] = float("inf")
         for rank in range(10):
             _index = np.argmin(dis[i])
             _label = labels[_index]
             if anchor_label == _label:
                 cmc.append(rank)
                 break
-            dis[i][_index] = float("inf")
+            diss[i][_index] = float("inf")
     rank10 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     for i in cmc:
         rank10[i] += 1/len(features)
@@ -32,6 +37,7 @@ def validation(features, labels):
     for i in range(9):
         rank10[i+1] += rank10[i]
     print("CMC {}".format(rank10))
+
 
 if __name__ == "__main__":
     test = torch.tensor(np.random.rand(10, 128))
